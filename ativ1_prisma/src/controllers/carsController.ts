52a8f49fd6df5cb493cecccdbbe737f0c9f455f2
j.vgms2@hotmail.com <j.vgms2@hotmail.com>
2025-08-17 11:47:51 -0300

    add: create crus cars

---
+++ ativ1_prisma/src/controllers/carsController.ts
@@ -0,0 +1,44 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient()
+
+export const getCar = async(req: Request, res: Response) => {
+    const cars = await prisma.carro.findMany()
+    res.json(cars)
+}
+
+export const createCars = async(req: Request, res: Response) => {
+    const { modelo, marca, ano } = req.body
+    const car = await prisma.carro.create({
+        data: {
+            modelo,
+            marca,
+            ano
+        }
+    })
+    res.status(201).json(car)
+}
+
+export const updateCars = async(req: Request, res: Response) => {
+    const { id } = req.params
+    const { modelo, marca, ano} = req.body
+
+    const newCar = await prisma.carro.update({
+        where: { id: Number(id) },
+        data: {
+            modelo,
+            marca,
+            ano
+        }
+    })
+    res.json(newCar)
+}
+
+export const deleteCars = async(req: Request, res: Response) => {
+    const { id } = req.params
+    await prisma.carro.delete({
+        where: { id: Number(id) }
+    })
+    res.status(204).send()
+}
