54a1e2136edae6297c8cf698a214173c67dc6f26
j.vgms2@hotmail.com <j.vgms2@hotmail.com>
2025-08-17 11:28:43 -0300

    add: create routes post,put,delete people

--- ativ1_prisma/src/routes/peopleRoutes.ts
+++ ativ1_prisma/src/routes/peopleRoutes.ts
@@ -1,8 +1,11 @@
 import express from "express"
-import { getPeople } from "../controllers/peopleController"
+import { createPeople, deletePeople, getPeople, updatePeople } from "../controllers/peopleController"
 
 const router = express.Router()
 
 router.get("/", getPeople)
+router.post("/cadastrarPessoa", createPeople)
+router.put("/editPessoa", updatePeople)
+router.delete("/delPessoa", deletePeople)
 
 export const peopleRoutes = router
